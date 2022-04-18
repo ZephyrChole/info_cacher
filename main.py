ed03a1447803771944117a9fb0d9b6887069c33e
ZephyrChole <1905616103@qq.com>
2022-04-18 19:50:51 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pickle
+import os
+from datetime import date
+
+
+class InfoCacher:
+    def __init__(self, dir_, name):
+        self.dir_ = dir_
+        self.name = name
+        self.history_file_path = os.path.join(self.dir_, self.get_history_filename(self.name))
+
+    def get_history_filename(self, name):
+        return '{}_history_{}.pickle'.format(name, self.get_today_str())
+
+    def get_today_str(self):
+        today = date.today()
+        return '{}{}{}'.format(str(today.year).rjust(2, '0'), str(today.month).rjust(2, '0'),
+                               str(today.day).rjust(2, '0'))
+
+    @property
+    def has_valid_cache(self):
+        return os.path.exists(self.history_file_path) and os.path.isfile(self.history_file_path)
+
+    def loads(self):
+        with open(self.history_file_path, 'rb') as file:
+            return pickle.loads(file.read())
+
+    def dumps(self, something):
+        with open(self.history_file_path, 'wb') as file:
+            file.write(pickle.dumps(something))
